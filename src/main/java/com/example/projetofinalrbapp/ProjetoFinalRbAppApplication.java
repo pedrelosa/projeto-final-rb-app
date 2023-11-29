3801f0b61d0da6b41904d29f9abc4f6d87bcc75f
Pedrelosa <pedrelosa@hotmail.com>
2023-11-28 22:18:19 -0300

    Initial commit

---
+++ src/main/java/com/example/projetofinalrbapp/ProjetoFinalRbAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetofinalrbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalRbAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoFinalRbAppApplication.class, args);
+    }
+
+}
