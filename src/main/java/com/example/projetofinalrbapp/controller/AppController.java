2f16512b8be1ed3a9189a789ca9de97fabfed27c
Pedrelosa <pedrelosa@hotmail.com>
2023-12-02 21:40:52 -0300

    Atualização para cadastro de estudantes

--- src/main/java/com/example/projetofinalrbapp/controller/AppController.java
+++ src/main/java/com/example/projetofinalrbapp/controller/AppController.java
@@ -5,7 +5,11 @@ import com.example.projetofinalrbapp.service.EstudanteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Comparator;
 import java.util.List;
@@ -26,6 +30,34 @@ public class AppController {
 
         return "index";
 
+    }
+
+    @RequestMapping("/novo")
+    public String cadastroEstudante(Model model){
+
+        Estudante estudante = new Estudante();
+        model.addAttribute("estudante", estudante);
+
+        return "cadastrar-estudante";
+    }
+
+
+    @RequestMapping(value = "/salvar", method = RequestMethod.POST)
+    public String salvarEstudante(@ModelAttribute("estudante") Estudante estudante){
+        service.adicionarEstudante(estudante);
+        return "redirect:/";
+    }
+
+
+    @RequestMapping("/editar/{id}")
+    public ModelAndView editarIdEstudante(@PathVariable(name = "id") long id){
+        ModelAndView mav = new ModelAndView("cadastrar-estudante");
+
+        Estudante estudante = service.modificar(id);
+
+        mav.addObject("estudante", estudante);
+
+        return mav;
     }
 
 }
